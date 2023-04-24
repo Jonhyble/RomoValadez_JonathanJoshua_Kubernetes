fb8e53005c4f886d00b091511043e128badd93af
Jonhyble <jonhy3-valadez@hotmail.com>
2023-04-24 02:04:53 -0600

    Primeros cambios

--- ApiPeliculas/src/main.py
+++ ApiPeliculas/src/main.py
@@ -1,32 +1,15 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from database import database as connection
 import requests
-
-from database import Administrador, Pelicula, Usuario, Generos, Usuario_Pelicula, Administrador_Pelicula, Genero_Pelicula
 
 app = FastAPI()
 
 @app.on_event("startup")
 def startup():
-    if connection.is_closed():
-        connection.connect()
-
-    connection.create_tables([  Administrador,
-                                Pelicula,
-                                Usuario,
-                                Generos,
-                                Usuario_Pelicula,
-                                Administrador_Pelicula,
-                                Genero_Pelicula])
     print("Bonjour")
 
 
 @app.on_event("shutdown")
 def shutdown():
-    if not connection.is_closed():
-        connection.close()
     print("Arrive Derchi")
 
 
